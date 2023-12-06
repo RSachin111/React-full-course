2b51d5ad9e19cb74a09562bb3b5aebf8914c1c3e
Sachin Rathor <sachinrathor880@gmail.com>
2023-12-06 18:53:51 +0530

    get data from apis

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -1,18 +1,34 @@
-// "use client"
-// import React, { useState } from 'react'
-import React from 'react'
-import Header from '@/Components/Header'
+"use client"
+import axios from 'axios';
+import React, { useState } from 'react';
+// import Header from '@/Components/Header';
 
-// useState
+const Page = () => {
+  const [images, setImages] = useState([]);
 
-const page = () => {
-  // const [user, setUser] = useState("sachin")
-  // const [career, setCareer] = useState('career')
+  const getImages = () => {
+    axios.get("https://picsum.photos/v2/list")
+      .then((response) => {
+        console.log(response.data);
+        setImages(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div> <Header />
-    This is home page 
+    // <div>
+    //   <Header />
+    //   {/* This is home page */}
+    // </div>
+    <div>
+      <button onClick={getImages}>Get images</button>
+      {images.map(image => (
+        <img key={image.id} src={image.download_url} alt={`Image ${image.id}`} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
